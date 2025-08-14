d59bcefdd79bfc6a218112ca3b55b74538f71634
Fire Coin <maxim.m.kollar@gmail.com>
2025-08-14 17:42:05 +0200

    Added text field for typing, and added letters on keys.

--- script.js
+++ script.js
@@ -8,14 +8,20 @@ let padding = 25;
 let cols = [10, 9, 7];
 let offsets = [0, width + padding / 2, 2 * width + padding + 20];
 
+let letter = 0;
+let layout = "QWERTYUIOPASDFGHJKLZXCVBNM";
+
 for (let row = 0; row < 3; row++) {
     for (let col = 0; col < cols[row]; col++) {
         const box = document.createElement("div");
         box.classList.add("key");
+        box.textContent = layout[letter];
+        
         box.style.left = `${col * (2 * width + padding) + offsets[row]}px`;
         box.style.top = `${row * (30 + 45)}px`
-
+        
         container.appendChild(box);
+        letter++;
     }
 }
 
